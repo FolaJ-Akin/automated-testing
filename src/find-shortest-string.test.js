c5167c949c58b60ecf8017e5609cbe520c50eede
Fola <jimiakinwale@icloud.com>
2022-02-23 16:32:12 +0000

    pushing tests failed

---
+++ src/find-shortest-string.test.js
@@ -0,0 +1,12 @@
+const findShortestString = require("./find-shortest-string");
+
+test("findShortestString finds the shortest string in an array", () => {
+  expect(findShortestString(["it", "is", "a", "nice", "day"])).toBe("a");
+  expect(findShortestString(["why", "hello", "to", "you"])).toBe("to");
+});
+
+test("findShortestString returns the earlier string in cases of joint shortest strings", () => {
+  expect(findShortestString(["brave", "dance"])).toBe("brave");
+  expect(findShortestString(["fola", "jim", "bingbong"])).toBe("jim");
+  expect(findShortestString(["l", "oo", "waga", "mama"])).toBe("l");
+});
